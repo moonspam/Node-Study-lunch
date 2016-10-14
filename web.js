58f4e270afe2b8f23dfb0ff1adbcd0e5a814eb8d
moonspam <booboo0412@gmail.com>
2016-10-14 16:04:09 +0900

    change name

--- web.js
+++ web.js
@@ -15,12 +15,12 @@ app.set("views", path.join(__dirname, "views"));
 // Middleware
 app.use("/static/", express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(morgan("dev")); // combined or dev
+app.use(morgan("combined")); // combined or dev
 
 // Router
 app.use("/", homeRouter);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8001;
 
 app.listen(port, function() {
     console.log("Server is running on http://localhost:" + port);
