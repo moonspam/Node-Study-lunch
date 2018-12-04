55e7a4be1c51c88a56cadfa7de48615e0b44826f
moonspam <booboo0412@gmail.com>
2018-12-04 15:35:47 +0900

    update pkg

--- middlewares/readcsv.js
+++ middlewares/readcsv.js
@@ -1,6 +1,6 @@
 /// <reference path="../typings/tsd.d.ts" />
 var fs = require("fs");
-var Converter = require("csvtojson").Converter;
+var Converter = require("csvtojson/v1").Converter;
 var converter = new Converter({});
 fs.createReadStream("./csv/store.csv").pipe(converter); // cafe24 url change /home/hosting_users/{{ your_cafe24_id }}/apps/{{ your_cafe24_app_name }}/csv/
 
